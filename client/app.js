77cf6fa928e8e3fbe737cf1c6294689460766764
Irving <Irvingb232@gmail.com>
2015-09-22 15:42:59 -0700

    1st commit

--- client/app.js
+++ client/app.js
@@ -1,23 +1,47 @@
 angular.module('irvingchat', [])
 
-.controller('UserController',['$scope', 'IrvingsData' function($scope, IrvingsData){
-	$scope.message = '';
+.controller('UserController',['$scope', 'IrvingsData', function($scope, IrvingsData){
 
-	$scope.sendMessage = function(){
+	// giving our controllers scope have all messages
+	$scope.messages = IrvingsData.messages;
+	console.log($scope.messages);
+
+	$scope.sendMessage = function(userName, message){
+		// sending the message
+		IrvingsData.myDataRef.push({name: userName, text: message});
 
 	};
 
-}]);
+
+}])
 
 .factory('IrvingsData', function(){
 	var myDataRef = new Firebase('https://ndtufcx866p.firebaseio-demo.com/');
+	var messages = [];
 
-	// sending the data
-	myDataRef.push({name: name, text: text});
-
-	// saving updated data
 	myDataRef.on('child_added', function(snapshot) {
+
+        // saving the message object
         var message = snapshot.val();
-        displayChatMessage(message.name, message.text);
+        // saving it into our own array
+        messages.push(message);
+        // keep only ten messages at a time.
+		deleteMessages();
     });
+
+    deleteMessages = function(){
+		var tooMany = true;
+		while(tooMany){
+			if(messages.length > 10){
+				messages.shift();
+			}else{
+				tooMany = false;
+			}
+		}
+	};
+
+    return {
+    	myDataRef: myDataRef,
+    	messages: messages
+    };
 })
